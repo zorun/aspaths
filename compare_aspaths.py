98e0e973388c0b637ae0b59c98ec6a484197e0ae
Baptiste Jonglez <git@bitsofnetworks.org>
2016-04-20 14:44:46 +0100

    Use prefix trees for BGP data, and handle MOAS
    Prefix trees improve RAM and CPU usage a lot.

--- compare_aspaths.py
+++ compare_aspaths.py
@@ -6,12 +6,10 @@ from collections import namedtuple
 import random
 
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
+from pytricia import PyTricia
 
 from iplane import IPlaneTraceFile, Hop
 import peeringdb
-
-
-BGPdata = namedtuple('BGPdata', ['prefix', 'as_path'])
 
 
 class ASPathsAnalyser(object):
@@ -21,8 +19,11 @@ class ASPathsAnalyser(object):
         self.source_asn = source_asn
 
     def load_ris(self, filename):
-        # TODO: faster datastructure
-        self.bgp = list()
+        """Loads a full RIS BGP table and store it in prefix trees for later use"""
+        # This maps each IP prefix to a set of origin AS (singleton except for MOAS)
+        self.bgp_origin = PyTricia()
+        # This maps each IP prefix to its AS path as seen by self.source_asn
+        self.bgp_aspath = PyTricia()
         record = BGPRecord()
         stream = BGPStream()
         stream.set_data_interface("singlefile")
@@ -37,9 +38,11 @@ class ASPathsAnalyser(object):
                     count += 1
                     # for testing: only pick one element in X
                     if count % 2 != 0:
+                    # Warning: this discards a lot of data!
                         break
-                    prefix = ip_network(elem.fields['prefix'].decode())
-                    if not isinstance(prefix, IPv4Network):
+                    prefix = elem.fields['prefix']
+                    # Discard IPv6 prefixes (crude but fast)
+                    if ':' in prefix:
                         elem = record.get_next_elem()
                         continue
                     if len(elem.fields['as-path']) == 0:
@@ -47,61 +50,50 @@ class ASPathsAnalyser(object):
                               file=sys.stderr)
                         elem = record.get_next_elem()
                         continue
+                    # First get the origin AS
+                    origin = elem.fields['as-path'].split(b' ')[-1]
                     # In rare cases, we have an as-set (BGP aggregation).
-                    # We simply take the first ASN for now.
-                    # TODO: possible MOAS
-                    as_path = [int(asn.strip(b'{}').split(b',')[0])
-                               for asn in elem.fields['as-path'].split(b' ')]
-                    d = BGPdata(prefix=prefix,
-                                as_path=as_path)
-                    self.bgp.append(d)
+                    if '{' in origin:
+                        origin = {int(asn) for asn in origin.strip(b'{}').split(b',')}
+                    else:
+                        origin = {int(origin)}
+                    if self.bgp_origin.has_key(prefix):
+                        self.bgp_origin[prefix].update(origin)
+                    else:
+                        self.bgp_origin[prefix] = origin
+                    # If received from self.source_asn, record the whole AS-path
+                    if elem.peer_asn == self.source_asn:
+                        # In rare cases, we have an as-set (BGP aggregation).
+                        # We simply take the first ASN for now.
+                        # TODO: possible MOAS
+                        as_path = [int(asn.strip(b'{}').split(b',')[0])
+                                   for asn in elem.fields['as-path'].split(b' ')]
+                        self.bgp_aspath[prefix] = as_path
                     elem = record.get_next_elem()
-            # For testing
-            #if count > 300000:
-            #    break
-        print("[RIS] Loaded {} BGP elements".format(len(self.bgp)),
+        print("[RIS] Loaded {} BGP prefixes in total".format(len(self.bgp_origin)),
+              file=sys.stderr)
+        print("[RIS] Loaded {} BGP prefixes from AS {}".format(len(self.bgp_aspath),
+                                                               self.source_asn),
               file=sys.stderr)
 
-    def ris_best_prefix_match(self, ip, first_asn=None):
-        """If [first_asn] is set, only look at BGP data with the specified ASN
-        as first ASN.
+    def ris_origin_asn(self, ip):
+        """Returns a set of origin ASN for the given IP address, by looking at
+        the most specific prefix covering the IP.  If no such prefix
+        exists, an empty set is returned.
         """
-        assert(isinstance(ip, (IPv4Address, IPv6Address)))
-        # TODO: faster algorithm
-        # TODO: MOAS handling
-        best_prefix = ip_network('0.0.0.0/0')
-        match = None
-        for elem in self.bgp:
-            if ip in elem.prefix:
-                #print("Found", ip, elem.prefix)
-                if elem.prefix.prefixlen > best_prefix.prefixlen:
-                    best_prefix = elem.prefix
-                    match = elem
-        return match
+        try:
+            return self.bgp_origin[ip]
+        except KeyError:
+            return set()
 
-    def ris_best_prefix_match_constrained(self, ip, first_asn):
-        """Only look at BGP data where the first ASN of the AS-path is equal
-        to [first_asn].
+    def ris_aspath_from_source(self, ip):
+        """Returns the AS-path for the most specific prefix seen by
+        self.source_asn.  If no prefix is found, None is returned.
         """
-        assert(isinstance(ip, (IPv4Address, IPv6Address)))
-        best_prefix = ip_network('0.0.0.0/0')
-        match = None
-        for elem in self.bgp:
-            if ip in elem.prefix:
-                if elem.as_path[0] != first_asn:
-                    continue
-                if elem.prefix.prefixlen > best_prefix.prefixlen:
-                    best_prefix = elem.prefix
-                    match = elem
-        return match
-
-    def ris_aspath(self, ip):
-        match = self.ris_best_prefix_match_constrained(ip, self.source_asn)
-        if match != None:
-            return match.as_path
-
-    def bgp_originator(self, bgp_elem):
-        return bgp_elem.as_path[-1]
+        try:
+            return self.bgp_aspath[ip]
+        except KeyError:
+            return None
 
     def load_peeringdb(self):
         p = peeringdb.PeeringDB('.')
@@ -121,24 +113,22 @@ class ASPathsAnalyser(object):
         return any((ip in prefix for prefix in self.ix_prefixes))
 
     def ip_to_asn(self, ip):
-        """Translate an IP address to an ASN, using peeringdb and RIS.
-        Returns None if the IP is part of an IX or if no ASN can be found."""
+        """Translate an IP address to a set of ASN, using peeringdb and RIS.
+        Returns empty set if the IP is part of an IX or if no ASN can be found."""
         # TODO: MOAS?
         # TODO: distinguish IXP and no match
         assert(isinstance(ip, (IPv4Address, IPv6Address)))
         # 0.0.0.0 represents an empty hop (and is an unusable IP anyway)
         if ip == self.IPV4_NULLADDRESS:
-            return None
+            return set()
         # Exact match in peeringdb
         if ip in self.ix_ip:
-            return self.ix_ip[ip]
+            return {self.ix_ip[ip]}
         # Prefix match in peeringdb
         if self.is_in_ix(ip):
-            return None
+            return set()
         # Best prefix match in RIS
-        bgp_elem = self.ris_best_prefix_match(ip)
-        if bgp_elem != None:
-            return self.bgp_originator(bgp_elem)
+        return self.ris_origin_asn(str(ip))
 
     def traceroute_aspath(self, traceroute):
         """Given a traceroute, compute an AS-path."""
@@ -146,8 +136,7 @@ class ASPathsAnalyser(object):
         for ip_str in [hop.ip for hop in traceroute.hops]:
             ip = ip_address(ip_str)
             asn = self.ip_to_asn(ip)
-            if asn != None:
-                aspath.append(asn)
+            aspath.append(asn)
         # TODO: post-process the raw AS-path
         return aspath
 
@@ -157,7 +146,7 @@ class ASPathsAnalyser(object):
             final_hop = Hop(traceroute.dest, 0., 0)
             traceroute.hops.append(final_hop)
         aspath = self.traceroute_aspath(traceroute)
-        bgp_aspath = self.ris_aspath(ip_address(traceroute.dest))
+        bgp_aspath = self.ris_aspath_from_source(traceroute.dest.encode())
         # Debug:
         print(' '.join([hop.ip for hop in traceroute.hops]))
         print(aspath)
