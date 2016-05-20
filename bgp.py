da70c9a50b13b9696394fc7376837adc768c43de
Baptiste Jonglez <git@bitsofnetworks.org>
2016-05-20 13:56:49 +0100

    Add possibility to load mrtdump update files

--- bgp.py
+++ bgp.py
@@ -3,13 +3,13 @@ from __future__ import print_function, unicode_literals, division
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 
 
-def load_rib_mrtdump(filename):
-    """Load BGP RIB data from a mrtdump file, and returns a generator on
+def load_mrtdump(filename, type):
+    """Load BGP data from a mrtdump file, and returns a generator on
     the BGP elements present in the file."""
     record = BGPRecord()
     stream = BGPStream()
     stream.set_data_interface("singlefile")
-    stream.set_data_interface_option("singlefile", "rib-file", filename)
+    stream.set_data_interface_option("singlefile", type, filename)
     # TODO: Allow additional filters to be passed
     stream.start()
     while(stream.get_next_record(record)):
@@ -18,3 +18,9 @@ def load_rib_mrtdump(filename):
             while(elem):
                 yield elem
                 elem = record.get_next_elem()
+
+def load_rib_mrtdump(filename):
+    return load_mrtdump(filename, "rib-file")
+
+def load_update_mrtdump(filename):
+    return load_mrtdump(filename, "upd-file")
